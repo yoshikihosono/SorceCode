afe565871c35e63be952bb145a150c9fd456c4df
yoshiki_hosono <yoshikihosono449@gmail.com>
2021-12-03 10:42:56 +0900

    アイテム(爆弾)のソースコードです

---
+++ Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    //爆発エフェクト格納用
+    public GameObject Explosion;
+
+    //当たったら
+    private void OnCollisionEnter(Collision collision)
+    {
+        //このオブジェクトを消します
+        Destroy(this.gameObject);
+        //爆発エフェクトをだします
+        Instantiate(Explosion, this.transform.position, Quaternion.identity);
+    }
+}
